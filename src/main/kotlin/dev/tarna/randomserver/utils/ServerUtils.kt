cd4e2c8cd764d863f445915b52a4b3906df62898
tarna <46247728+tarna@users.noreply.github.com>
2024-11-02 16:01:23 -0400

    fix: update link

--- src/main/kotlin/dev/tarna/randomserver/utils/ServerUtils.kt
+++ src/main/kotlin/dev/tarna/randomserver/utils/ServerUtils.kt
@@ -6,7 +6,7 @@ import io.ktor.client.call.*
 import io.ktor.client.request.*
 
 suspend fun fetchServers() {
-    servers = client.get("https://github.com/tarna/minehut/blob/master/servers.json").body<List<Server>>()
+    servers = client.get("https://github.com/tarna/random-server/blob/main/servers.json").body<List<Server>>()
 }
 
 fun randomServer(): Server {
